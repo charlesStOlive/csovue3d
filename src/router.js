002132e262bdf8624f972d5016badc77a2985cf4
charles <charles.stolive@dayonmars.fr>
2019-07-23 18:20:21 +0200

    travail sur middleware

--- src/router.js
+++ src/router.js
@@ -1,17 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+//
+import store from './store'
+//
+import guest from './middleware/guest'
+import auth from './middleware/auth'
+import isSubscribed from './middleware/isSubscribed'
+import middlewarePipeline from './middlewarePipeline'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        middleware: [
+          guest
+        ]
+      }
     },
     {
       path: '/about',
@@ -19,7 +30,30 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.middleware) {
+    return next()
+  }
+  const middleware = to.meta.middleware
+
+  const context = {
+    to,
+    from,
+    next,
+    store
+  }
+
+
+  return middleware[0]({
+    ...context,
+    next: middlewarePipeline(context, middleware, 1)
+  })
+
+})
+
+export default router
